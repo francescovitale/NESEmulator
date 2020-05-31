bc2024228440e9c7d8a83cdc8249eb5db7c43737
francescovitale <64461360+francescovitale@users.noreply.github.com>
2020-05-31 19:15:09 +0200

    Add files via upload

---
+++ code/Control/Controller.java
@@ -0,0 +1,15 @@
+package Control;
+
+public class Controller {
+
+	/**
+	 * 
+	 * @param Nome
+	 * @param ID
+	 */
+	public void executeProgram(String Nome, Int ID) {
+		// TODO - implement Controller.executeProgram
+		throw new UnsupportedOperationException();
+	}
+
+}
