61d75c1c7938cf7b6fd115238b0777bb945e6fd3
francescovitale <64461360+francescovitale@users.noreply.github.com>
2020-05-31 19:14:34 +0200

    Add files via upload

---
+++ code/ApplicationLogic/EmulatorFacade.java
@@ -0,0 +1,26 @@
+package ApplicationLogic;
+
+import ApplicationLogic.Interpreter.*;
+import ApplicationLogic.State.*;
+
+public class EmulatorFacade {
+
+	ControlUnit ControlUnit;
+	Mapper Mapper;
+
+	/**
+	 * 
+	 * @param ROMName
+	 * @param ID
+	 */
+	public Boolean initProgram(String ROMName, Integer ID) {
+		// TODO - implement EmulatorFacade.initProgram
+		throw new UnsupportedOperationException();
+	}
+
+	public Boolean startCycle() {
+		// TODO - implement EmulatorFacade.startCycle
+		throw new UnsupportedOperationException();
+	}
+
+}
