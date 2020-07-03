1b523c5e8071777e32d4b53eebcced42ff047fb6
francescovitale <64461360+francescovitale@users.noreply.github.com>
2020-07-03 10:40:20 +0200

    Delete ControlUnitState.java

--- code/ApplicationLogic/Interpreter/ControlUnitState.java
+++
@@ -1,9 +0,0 @@
-package ApplicationLogic.Interpreter;
-
-public abstract class ControlUnitState {
-
-	protected abstract void changeState(ControlUnit CU, ControlUnitState NewState);
-
-	public abstract void execCycle();
-
-}
