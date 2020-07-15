384aa40facbb98c2b57347a34ae3540bf7e21e00
francescovitale <64461360+francescovitale@users.noreply.github.com>
2020-07-15 21:26:43 +0200

    Add files via upload

---
+++ code/Emulator/TechnicalServices/AccessProgramFS.java
@@ -0,0 +1,25 @@
+package Emulator.TechnicalServices;
+
+import java.util.ArrayList;
+
+import Emulator.ApplicationLogic.Program;
+import Emulator.TechnicalServices.FileSystemAccess.FileSystemManager;
+
+public class AccessProgramFS extends AccessProgram{
+	
+	FileSystemManager Fsm;
+	
+	public AccessProgramFS() {};
+	
+	public ArrayList<Program> loadProgram(String SelectedPath) {
+		Fsm = FileSystemManager.getInstance();
+		String TempFileType = SelectedPath.substring(SelectedPath.length()-4,SelectedPath.length());
+		if(TempFileType.equals("ines") || TempFileType.equals("nes"))
+			return new ArrayList<Program>(Fsm.getProgram(SelectedPath, 0));
+		else
+			return new ArrayList<Program>(Fsm.getProgram(SelectedPath, 1));
+		
+	};
+
+}
+ 
