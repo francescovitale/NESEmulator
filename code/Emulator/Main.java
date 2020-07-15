3bd703b822e2a7fc6e1c0f79f25aeeb4317b5c31
francescovitale <64461360+francescovitale@users.noreply.github.com>
2020-07-16 00:11:01 +0200

    Add files via upload

--- code/Emulator/Main.java
+++ code/Emulator/Main.java
@@ -8,10 +8,10 @@ public class Main {
 	public static void main(String args[]) {
 		
 		Controller C = new Controller();
-		String Nome = "GIOCO";
+		String Nome = "GIOCOo";
 
 		
-		C.UpdateProgram("Pippo","MarioBros", 3  );
+		//C.UpdateProgram("Pippo","MarioBros", 3);
 	
 		/*FileSystemManager FSM = FileSystemManager.getInstance();
 		FSM.deleteFile("C:\\Users\\aceep\\eclipse-workspace\\NES\\src\\Emulator\\Log\\log.txt");*/
@@ -20,6 +20,10 @@ public class Main {
 		//C.DumpMemory();
 		//Byte a = 0x10;
 		//Byte b = 
+		//C.InsertProgram("C:\\Users\\aceep\\eclipse-workspace\\NES\\src\\Emulator\\programmi di prova\\nestest.ines","",-1);
+		
+		C.executeProgram("nestest",1,"");
+		
 		
 		
 	}
